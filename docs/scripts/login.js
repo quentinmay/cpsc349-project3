8c9717dfab5ec7d85b75cdd3821b98232aafec7f
quentinmay <quentinemay@gmail.com>
2021-07-28 18:23:49 -0700

    Added signout utility

--- docs/scripts/login.js
+++ docs/scripts/login.js
@@ -1,10 +1,10 @@
 import * as mockroblog from './mockroblog.js'
+import * as utility from "./util.js"
 
 window.userLogin = function userLogin() {
     let username = document.getElementById('username').value;
     let password = document.getElementById('password').value;
     let user = mockroblog.authenticateUser(username, password);
-    password
     if (user) {
         document.cookie = "userID=" + user.id + '; expires=' + new Date(2147483647 * 1000).toUTCString();
         alert("Login succeeded");
@@ -13,5 +13,12 @@ window.userLogin = function userLogin() {
     } else {
         console.log(username, password, user);
         alert("Login failed.");
+    }
+}
+
+window.userLogout = function userLogout() {
+    if (utility.isLoggedIn()) {
+        document.cookie = "userID=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        window.location.replace("/login.html");
     }
 }
