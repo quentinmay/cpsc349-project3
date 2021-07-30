30f59107a7349d0863fc35d97af4c79a30ca23b2
quentinmay <quentinemay@gmail.com>
2021-07-29 20:30:24 -0700

    Added placeholder images

--- docs/scripts/initTimeline.js
+++ docs/scripts/initTimeline.js
@@ -68,11 +68,8 @@ function appendPosts(timelineJson) {
         */
         let newPost = document.createElement('div');
         newPost.innerHTML = `                    <div class="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
-    <div class="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
-      <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="sm:w-16 sm:h-16 w-10 h-10" viewBox="0 0 24 24">
-        <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-      </svg>
-    </div>
+    <img src="https://via.placeholder.com/150/3b82f6/FFFFFF?text=${(mockroblog.getUserName(post.user_id))}" class="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
+    </img>
     <div class="flex-grow sm:text-left text-center mt-6 sm:mt-0">
       <h2 class="text-gray-900 text-lg title-font font-medium mb-2">${(mockroblog.getUserName(post.user_id))}</h2>
       <p class="leading-relaxed text-base">${post.text}</p>
