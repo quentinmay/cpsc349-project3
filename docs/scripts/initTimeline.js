d73c8ce6e81530b361fee2f6b9c868dfdcaabc13
stephaniemariecobb <55161389+stephaniemariecobb@users.noreply.github.com>
2021-07-30 15:12:20 -0700

    Follow button text changes

--- docs/scripts/initTimeline.js
+++ docs/scripts/initTimeline.js
@@ -12,31 +12,46 @@ function onBoot() {
     }
 }
 
+// Buttons
 let userBtn = document.getElementById('user-button');
 let homeBtn = document.getElementById('home-button');
 let publicBtn = document.getElementById('public-button');
+let followButn = document.getElementById('follow-button');
+let formerBtn = 'Unfollow';
 
+// Follow Button
+followButn.addEventListener('click', () => {
+  const tmpBtn = followButn.innerHTML;
+  followButn.innerHTML = formerBtn;
+  formerBtn = tmpBtn;
+});
+
+// User Timeline Button
 userBtn.addEventListener('click', () => {
     let user = localStorage.getItem("username");
     if (user) {
         const timeline = mockroblog.getUserTimeline(user);
         appendPosts(timeline);
-
     }
-
 });
 
+// Home Timeline Button
 homeBtn.addEventListener('click', () => {
     let user = localStorage.getItem("username");
     if (user) {
         appendPosts(mockroblog.getHomeTimeline(user));
-
     }
 });
 
+// Public Timeline Button
 publicBtn.addEventListener('click', () => {
     appendPosts(mockroblog.getPublicTimeline());
 });
+
+// Follow Button
+//followBtn.addEventListener('click', () => {
+
+//});
 
 function populateTimeline() {
     appendPosts(mockroblog.getPublicTimeline());
@@ -64,20 +79,18 @@ function appendPosts(timelineJson) {
                     </div>
                 </div>
         */
-        let newPost = document.createElement('div');
-        newPost.innerHTML = `<div class="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
+      let newPost = document.createElement('div');
+      newPost.innerHTML = `<div class="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
         <img src="https://via.placeholder.com/150/0492C2/FFFFFF?text=${(mockroblog.getUserName(post.user_id))}" class="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0"></img>
-        <div class="flex-grow sm:text-left text-center mt-6 sm:mt-0">
-          <h2 class="text-gray-900 text-lg title-font font-medium mb-2">${(mockroblog.getUserName(post.user_id))}</h2>
-          <p class="leading-relaxed text-base">${post.text}</p>
-          <a class="mt-3 text-indigo-500 inline-flex items-center">${post.timestamp}
-          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-            <path d="M5 12h14M12 5l7 7-7 7"></path>
-          </svg>
-          </a>
-        </div>
-      </div>`
-        posts.appendChild(newPost);
+          <div class="flex-grow sm:text-left text-center mt-6 sm:mt-0">
+            <h2 class="text-gray-900 text-lg title-font font-medium mb-2">${(mockroblog.getUserName(post.user_id))}</h2>
+            <p class="leading-relaxed text-base">${post.text}</p>
+            <a class="mt-3 text-black-500 inline-flex items-center">${post.timestamp}</a>
+            <button class="hyperlink px-8 py-2" id="follow-button">Follow</button>
+          </div>
+      </div>
+      `
+      posts.appendChild(newPost);
     }
     return;
 }
