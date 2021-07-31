caed1cbda26146f11462c557a9801a62e1f04857
quentinmay <quentinemay@gmail.com>
2021-07-31 13:41:52 -0700

    Added "follow" button to timeline

--- docs/scripts/initTimeline.js
+++ docs/scripts/initTimeline.js
@@ -21,9 +21,9 @@ let formerBtn = 'Unfollow';
 
 // Follow Button
 followButn.addEventListener('click', () => {
-  const tmpBtn = followButn.innerHTML;
-  followButn.innerHTML = formerBtn;
-  formerBtn = tmpBtn;
+    const tmpBtn = followButn.innerHTML;
+    followButn.innerHTML = formerBtn;
+    formerBtn = tmpBtn;
 });
 
 // User Timeline Button
@@ -79,18 +79,28 @@ function appendPosts(timelineJson) {
                     </div>
                 </div>
         */
-      let newPost = document.createElement('div');
-      newPost.innerHTML = `<div class="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
-        <img src="https://via.placeholder.com/150/0492C2/FFFFFF?text=${(mockroblog.getUserName(post.user_id))}" class="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0"></img>
-          <div class="flex-grow sm:text-left text-center mt-6 sm:mt-0">
-            <h2 class="text-gray-900 text-lg title-font font-medium mb-2">${(mockroblog.getUserName(post.user_id))}</h2>
-            <p class="leading-relaxed text-base">${post.text}</p>
-            <a class="mt-3 text-black-500 inline-flex items-center">${post.timestamp}</a>
-            <button class="hyperlink px-8 py-2" id="follow-button">Follow</button>
-          </div>
-      </div>
-      `
-      posts.appendChild(newPost);
+        let newPost = document.createElement('div');
+        newPost.innerHTML = `<div class="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
+            <img src="https://via.placeholder.com/150/0492C2/FFFFFF?text=${(mockroblog.getUserName(post.user_id))}" class="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0"></img>
+            <div class="flex-grow sm:text-left text-center mt-6 sm:mt-0">
+                <h2 class="text-gray-900 text-lg title-font font-medium mb-2">${(mockroblog.getUserName(post.user_id))}</h2>
+                <p class="leading-relaxed text-base">${post.text}</p>
+                <a class="mt-3 text-black-500 inline-flex items-center">${post.timestamp}</a>
+                <button class="hyperlink px-8 py-2" id="follow-button">Follow</button>
+            </div>
+        </div>
+        `
+
+        //Add follower
+        let followBtn = newPost.children[0].children[1].children[3];
+        followBtn.addEventListener('click', () => {
+            let loggedInUser = localStorage.getItem("userID");
+            if (loggedInUser && post.user_id) {
+                mockroblog.addFollower(loggedInUser, post.user_id)
+                console.log(`Added follower: ${post.user_id}`);
+            }
+        });
+        posts.appendChild(newPost);
     }
     return;
 }
