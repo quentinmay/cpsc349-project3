a69f7d55d590ed640fd9325eb72dc1aaaf013136
quentinmay <quentinmay@users.noreply.github.com>
2021-08-01 02:44:45 +0000

    deploy: 3ab5cd3e774960b47362c91f3054e4518eb8be48

--- scripts/login.js
+++ scripts/login.js
@@ -1,15 +1,9 @@
 import * as mockroblog from './mockroblog.js'
 import * as utility from "./util.js"
 
-window.userLogout = function userLogout() {
-    if (utility.isLoggedIn()) {
-        localStorage.removeItem('userID');
-        window.location.replace("/login.html");
-    }
-}
 
-let button = document.getElementById('loginButton');
-button.addEventListener('click', () => {
+let loginButton = document.getElementById('loginButton');
+loginButton.addEventListener('click', () => {
     let username = document.getElementById('usernameN').value;
     let password = document.getElementById('passwordN').value;
     let user = mockroblog.authenticateUser(username, password);
@@ -26,28 +20,33 @@ button.addEventListener('click', () => {
     }
 });
 
-let button2 = document.getElementById('registerButton');
-button2.addEventListener('click', () => {
+let registerButton = document.getElementById('registerButton');
+registerButton.addEventListener('click', () => {
     let username = document.getElementById('regUsername').value;
     let password = document.getElementById('regPassword').value;
-    let email    = document.getElementById('email').value;
+    let email = document.getElementById('email').value;
+    let createdUser = mockroblog.createUser(username, email, password);
 
-    if(mockroblog.createUser(username, email, password)){
+    if (createdUser) {
         alert("Account successfully created. Please Login");
+        localStorage.setItem('userID', createdUser.id);
+        localStorage.setItem('username', createdUser.username);
+        window.location.replace(`${document.URL.substr(0, document.URL.lastIndexOf('/'))}/index.html`)
+
     }
-    else{
+    else {
         alert("An Account with that information already exists. Please try again.");
     }
 });
 
 let button3 = document.getElementById('login-link')
-button3.addEventListener('click', () =>{
-    document.getElementById('register-container').style.visibility="hidden";
-    document.getElementById('login-container').style.visibility="visible";
+button3.addEventListener('click', () => {
+    document.getElementById('register-container').hidden = true;
+    document.getElementById('login-container').hidden = false;
 });
 
 let button4 = document.getElementById('signup-link')
-button4.addEventListener('click', () =>{
-    document.getElementById('register-container').style.visibility="visible";
-    document.getElementById('login-container').style.visibility="hidden";
+button4.addEventListener('click', () => {
+    document.getElementById('register-container').hidden = false;
+    document.getElementById('login-container').hidden = true;
 });
