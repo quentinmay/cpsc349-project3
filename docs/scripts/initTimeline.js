cee842c7d96d08a12f7fdbc1e76173ce2c2e4171
stephaniemariecobb <55161389+stephaniemariecobb@users.noreply.github.com>
2021-07-31 20:28:05 -0700

    Ran standard --fix to fix small .js errors

--- docs/scripts/initTimeline.js
+++ docs/scripts/initTimeline.js
@@ -1,71 +1,71 @@
 import * as mockroblog from './mockroblog.js'
-import * as utility from "./util.js"
+import * as utility from './util.js'
 
 window.mockroblog = mockroblog
-onBoot();
-function onBoot() {
-    let loggedIn = utility.isLoggedIn();
-    if (loggedIn) {
-        populateTimeline();
-    } else { // If not logged in, take them back to the log-in page
-        //https://stackoverflow.com/questions/16984943/how-to-get-the-directory-part-of-current-url-in-javascript/16985051
-        window.location.replace(`${document.URL.substr(0, document.URL.lastIndexOf('/'))}/login.html`)
-    }
+onBoot()
+function onBoot () {
+  const loggedIn = utility.isLoggedIn()
+  if (loggedIn) {
+    populateTimeline()
+  } else { // If not logged in, take them back to the log-in page
+    // https://stackoverflow.com/questions/16984943/how-to-get-the-directory-part-of-current-url-in-javascript/16985051
+    window.location.replace(`${document.URL.substr(0, document.URL.lastIndexOf('/'))}/login.html`)
+  }
 }
 
 // Buttons
-let userBtn = document.getElementById('user-button');
-let homeBtn = document.getElementById('home-button');
-let publicBtn = document.getElementById('public-button');
-let postBtn = document.getElementById('post-button');
-let logoutBtn = document.getElementById('logout-button');
+const userBtn = document.getElementById('user-button')
+const homeBtn = document.getElementById('home-button')
+const publicBtn = document.getElementById('public-button')
+const postBtn = document.getElementById('post-button')
+const logoutBtn = document.getElementById('logout-button')
 
 // Logout Button
 logoutBtn.addEventListener('click', () => {
-    utility.userLogout();
-});
+  utility.userLogout()
+})
 
 // Post Message Button
 postBtn.addEventListener('click', () => {
-    var postMsg = prompt("Please provide a post", "Today I experienced...");
-    if (postMsg != "" && postMsg != null) {
-        let user = localStorage.getItem("userID")
-        mockroblog.postMessage(user, postMsg);
-        alert("You have posted a new message.");
-    }
-});
+  const postMsg = prompt('Please provide a post', 'Today I experienced...')
+  if (postMsg != '' && postMsg != null) {
+    const user = localStorage.getItem('userID')
+    mockroblog.postMessage(user, postMsg)
+    alert('You have posted a new message.')
+  }
+})
 
 // User Timeline Button
 userBtn.addEventListener('click', () => {
-    let user = localStorage.getItem("username");
-    if (user) {
-        const timeline = mockroblog.getUserTimeline(user);
-        appendPosts(timeline);
-    }
-});
+  const user = localStorage.getItem('username')
+  if (user) {
+    const timeline = mockroblog.getUserTimeline(user)
+    appendPosts(timeline)
+  }
+})
 
 // Home Timeline Button
 homeBtn.addEventListener('click', () => {
-    let user = localStorage.getItem("username");
-    if (user) {
-        appendPosts(mockroblog.getHomeTimeline(user));
-    }
-});
+  const user = localStorage.getItem('username')
+  if (user) {
+    appendPosts(mockroblog.getHomeTimeline(user))
+  }
+})
 
 // Public Timeline Button
 publicBtn.addEventListener('click', () => {
-    appendPosts(mockroblog.getPublicTimeline());
-});
+  appendPosts(mockroblog.getPublicTimeline())
+})
 
-function populateTimeline() {
-    appendPosts(mockroblog.getPublicTimeline());
+function populateTimeline () {
+  appendPosts(mockroblog.getPublicTimeline())
 }
 
-function appendPosts(timelineJson) {
-    let posts = document.querySelector('#post-container');
-    posts.innerHTML = "";
-    for (let post of timelineJson) {
-        /*
+function appendPosts (timelineJson) {
+  const posts = document.querySelector('#post-container')
+  posts.innerHTML = ''
+  for (const post of timelineJson) {
+    /*
                 <div class="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
                     <div class="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
                       <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="sm:w-16 sm:h-16 w-10 h-10" viewBox="0 0 24 24">
@@ -83,9 +83,9 @@ function appendPosts(timelineJson) {
                     </div>
                 </div>
         */
-        let newPost = document.createElement('div');
-        newPost.className = "post-item";
-        newPost.innerHTML = `<div class="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
+    const newPost = document.createElement('div')
+    newPost.className = 'post-item'
+    newPost.innerHTML = `<div class="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
             <img src="https://via.placeholder.com/150/0492C2/FFFFFF?text=${(mockroblog.getUserName(post.user_id))}" class="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0"></img>
             <div class="flex-grow sm:text-left text-center mt-6 sm:mt-0">
                 <h2 class="post-username text-gray-900 text-lg title-font font-medium mb-2">${(mockroblog.getUserName(post.user_id))}</h2>
@@ -96,37 +96,35 @@ function appendPosts(timelineJson) {
         </div>
         `
 
-        //Add follower
-        let followBtn = newPost.children[0].children[1].children[3];
-        followBtn.addEventListener('click', () => {
-            let loggedInUser = localStorage.getItem("userID");
-            if (followBtn.textContent === "Follow") {
-                if (loggedInUser && post.user_id) {
-                    mockroblog.addFollower(loggedInUser, post.user_id)
-                    console.log(`Added follower: ${post.user_id}`);
-                    updateTimeline(true, post.user_id);
-
-                }
-            } else if (followBtn.textContent === "Unfollow") {
-                if (loggedInUser && post.user_id) {
-                    mockroblog.removeFollower(loggedInUser, post.user_id)
-                    console.log(`Removed follower: ${post.user_id}`);
-                    updateTimeline(false, post.user_id);
-                }
-            }
-        });
-        posts.appendChild(newPost);
-    }
-    return;
+    // Add follower
+    const followBtn = newPost.children[0].children[1].children[3]
+    followBtn.addEventListener('click', () => {
+      const loggedInUser = localStorage.getItem('userID')
+      if (followBtn.textContent === 'Follow') {
+        if (loggedInUser && post.user_id) {
+          mockroblog.addFollower(loggedInUser, post.user_id)
+          console.log(`Added follower: ${post.user_id}`)
+          updateTimeline(true, post.user_id)
+        }
+      } else if (followBtn.textContent === 'Unfollow') {
+        if (loggedInUser && post.user_id) {
+          mockroblog.removeFollower(loggedInUser, post.user_id)
+          console.log(`Removed follower: ${post.user_id}`)
+          updateTimeline(false, post.user_id)
+        }
+      }
+    })
+    posts.appendChild(newPost)
+  }
 }
 
-function updateTimeline(follow, userID) {
-    let postItems = document.querySelector('#post-container').getElementsByClassName("post-item");
-    for (let postItem of postItems) {
-        console.log(postItem.getElementsByClassName("post-username"));
-        if (postItem.getElementsByClassName("post-username")[0].textContent === mockroblog.getUserName(userID)) {
-            let followBtn = postItem.children[0].children[1].children[3];
-            followBtn.textContent = (follow ? "Unfollow" : "Follow");
-        }
+function updateTimeline (follow, userID) {
+  const postItems = document.querySelector('#post-container').getElementsByClassName('post-item')
+  for (const postItem of postItems) {
+    console.log(postItem.getElementsByClassName('post-username'))
+    if (postItem.getElementsByClassName('post-username')[0].textContent === mockroblog.getUserName(userID)) {
+      const followBtn = postItem.children[0].children[1].children[3]
+      followBtn.textContent = (follow ? 'Unfollow' : 'Follow')
     }
+  }
 }
--- docs/scripts/login.js
+++ docs/scripts/login.js
@@ -1,52 +1,48 @@
 import * as mockroblog from './mockroblog.js'
-import * as utility from "./util.js"
+import * as utility from './util.js'
 
-
-let loginButton = document.getElementById('loginButton');
+const loginButton = document.getElementById('loginButton')
 loginButton.addEventListener('click', () => {
-    let username = document.getElementById('usernameN').value;
-    let password = document.getElementById('passwordN').value;
-    let user = mockroblog.authenticateUser(username, password);
-    if (user) {
-        localStorage.setItem('userID', user.id);
-        localStorage.setItem('username', user.username);
-        alert("Login succeeded");
-        //https://stackoverflow.com/questions/16984943/how-to-get-the-directory-part-of-current-url-in-javascript/16985051
-        window.location.replace(`${document.URL.substr(0, document.URL.lastIndexOf('/'))}/index.html`)
+  const username = document.getElementById('usernameN').value
+  const password = document.getElementById('passwordN').value
+  const user = mockroblog.authenticateUser(username, password)
+  if (user) {
+    localStorage.setItem('userID', user.id)
+    localStorage.setItem('username', user.username)
+    alert('Login succeeded')
+    // https://stackoverflow.com/questions/16984943/how-to-get-the-directory-part-of-current-url-in-javascript/16985051
+    window.location.replace(`${document.URL.substr(0, document.URL.lastIndexOf('/'))}/index.html`)
+  } else {
+    console.log(username, password, user)
+    alert('Login failed.')
+  }
+})
 
-    } else {
-        console.log(username, password, user);
-        alert("Login failed.");
-    }
-});
-
-let registerButton = document.getElementById('registerButton');
+const registerButton = document.getElementById('registerButton')
 registerButton.addEventListener('click', () => {
-    let username = document.getElementById('regUsername').value;
-    let password = document.getElementById('regPassword').value;
-    let email = document.getElementById('email').value;
-    let createdUser = mockroblog.createUser(username, email, password);
+  const username = document.getElementById('regUsername').value
+  const password = document.getElementById('regPassword').value
+  const email = document.getElementById('email').value
+  const createdUser = mockroblog.createUser(username, email, password)
 
-    if (createdUser) {
-        alert("Account successfully created. Please Login");
-        localStorage.setItem('userID', createdUser.id);
-        localStorage.setItem('username', createdUser.username);
-        window.location.replace(`${document.URL.substr(0, document.URL.lastIndexOf('/'))}/index.html`)
+  if (createdUser) {
+    alert('Account successfully created. Please Login')
+    localStorage.setItem('userID', createdUser.id)
+    localStorage.setItem('username', createdUser.username)
+    window.location.replace(`${document.URL.substr(0, document.URL.lastIndexOf('/'))}/index.html`)
+  } else {
+    alert('An Account with that information already exists. Please try again.')
+  }
+})
 
-    }
-    else {
-        alert("An Account with that information already exists. Please try again.");
-    }
-});
-
-let button3 = document.getElementById('login-link')
+const button3 = document.getElementById('login-link')
 button3.addEventListener('click', () => {
-    document.getElementById('register-container').hidden = true;
-    document.getElementById('login-container').hidden = false;
-});
+  document.getElementById('register-container').hidden = true
+  document.getElementById('login-container').hidden = false
+})
 
-let button4 = document.getElementById('signup-link')
+const button4 = document.getElementById('signup-link')
 button4.addEventListener('click', () => {
-    document.getElementById('register-container').hidden = false;
-    document.getElementById('login-container').hidden = true;
-});
+  document.getElementById('register-container').hidden = false
+  document.getElementById('login-container').hidden = true
+})
